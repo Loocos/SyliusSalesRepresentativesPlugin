1cf9e2cf779dd04068023383081a0c4b9e036a71
Octav <octav@octav.name>
2021-06-11 13:10:51 +0300

    Remove compatibility for older versions of Symfony & Sylius

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -20,13 +20,7 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('tavy315_sylius_sales_representatives');
-        if (\method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('tavy315_sylius_sales_representatives');
-        }
-
+        $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->addDefaultsIfNotSet()
             ->children()
